a35e8962e7a5395c3f9f8594408c2adcd4945944
Greg Mahring <greg.mahring@leandogsoftware.com>
2018-02-06 14:53:56 -0500

    convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Greg Mahring on 2/6/18.
+//  Copyright © 2018 Greg Mahring. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
